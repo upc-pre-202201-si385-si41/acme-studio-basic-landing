938e8343113b07b93b90c921b8a348288a620628
Angel Velasquez <pcisavel@upc.edu.pe>
2022-06-01 08:52:55 -0500

    feat(index, tos): Implemented basic structure.

---
+++ public/assets/scripts/main.js
@@ -0,0 +1,6 @@
+$.get("_header.html", contentToInject => {
+    $("#header-placeholder").replaceWith(contentToInject);
+});
+$.get("_footer.html", contentToInject => {
+    $("#footer-placeholder").replaceWith(contentToInject);
+});
